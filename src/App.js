c103e51374159b473a9d0e79f070afa542e50621
eullerpeixoto <eullerz01@hotmail.com>
2021-05-25 22:42:58 -0300

    80% pronto, faltando terminar a função de Edit e construir a UI.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = React.useState('')
+  const [list, setList] = React.useState([])
+
+  const handleClick = () => {
+  if(input === '') return
+  if(list.includes(input)) return
+  setList([...list, {todo: `${input}`}])
+  setInput('')
+}
+
+  const handleChange = ({target}) => {
+    setInput(target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={input}></input>
+        <button type="submit" onClick={handleClick}>Enviar</button>
+      </form>
+
+      <ul>
+      {list.map((lista, index) =>
+        <li key={index}>
+        {lista.todo}
+        <button
+        type="button"
+        onClick={() => setList(list.filter((e) => e !== lista))}>
+        Excluir
+      </button>
+      <button
+        type="button" onClick={() => lista.todo = 'ola'}>
+        Editar
+      </button>
+        </li>
+      )}
+     </ul>
+
     </div>
   );
 }
